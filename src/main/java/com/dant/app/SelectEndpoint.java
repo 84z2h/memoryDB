a139c3c78a2d9449752df050fa62ecc1f5d1e64b
84z2h <90423456+84z2h@users.noreply.github.com>
2022-05-11 16:40:11 +0200

    delete select String

--- src/main/java/com/dant/app/SelectEndpoint.java
+++ src/main/java/com/dant/app/SelectEndpoint.java
@@ -12,29 +12,15 @@ import java.io.IOException;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes({MediaType.APPLICATION_JSON,MediaType.TEXT_PLAIN, MediaType.MULTIPART_FORM_DATA})
 public class SelectEndpoint {
-/*
     @GET
     @Path("/{table}/{columns}")
-    public String select(@PathParam("table") String tableParam,@PathParam("columns") String columns) throws IOException {
-        return BasicStorage.select(tableParam, columns);
-    }
-*/
-
-    @GET
-    @Path("/{table}/{columns}")
-    public ResultSet selectJson(@PathParam("table") String tableParam, @PathParam("columns") String columns) throws IOException {
+    public ResultSet select(@PathParam("table") String tableParam, @PathParam("columns") String columns) throws IOException {
         return BasicStorage.selectJson(tableParam, columns);
     }
-/*
+
     @GET
     @Path("/{table}/{columns}/{whereclause}")
-    public String where(@PathParam("table") String table, @PathParam("columns") String columns, @PathParam("whereclause") String whereclause){
-        return BasicStorage.select_where(table, columns,whereclause);
-    }
-*/
-    @GET
-    @Path("/{table}/{columns}/{whereclause}")
-    public ResultSet whereJson(@PathParam("table") String table, @PathParam("columns") String columns, @PathParam("whereclause") String whereclause){
+    public ResultSet where(@PathParam("table") String table, @PathParam("columns") String columns, @PathParam("whereclause") String whereclause){
         return BasicStorage.select_whereJson(table, columns,whereclause);
     }
 
