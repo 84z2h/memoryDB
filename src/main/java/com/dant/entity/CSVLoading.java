09cce66b069f3dc7ef3302060b3cd929826f06c4
Abdou78130 <abdallah2901@gmail.com>
2022-05-28 19:03:14 +0200

    Insert method updated (but not used)

--- src/main/java/com/dant/entity/CSVLoading.java
+++ src/main/java/com/dant/entity/CSVLoading.java
@@ -1,13 +1,13 @@
 package com.dant.entity;
 
-import com.dant.entity.columns.Column;
+import com.dant.entity.columns.*;
 import com.dant.storage.BasicStorage;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class CSVLoading {
@@ -53,7 +53,6 @@ public class CSVLoading {
     }
 
     public static String insertTable(String csv_filename, int max_size, InputStream input) throws IOException {
-        // MODE == 0 -> Creation de table
         System.out.println("start loading data");
         BufferedReader in = new BufferedReader(new InputStreamReader(input));
         String s = null;
@@ -92,4 +91,91 @@ public class CSVLoading {
         in.close();
         return Time;
     }
+
+    public static String insertTable2(String csv_filename, int max_size, InputStream input) throws IOException {
+        System.out.println("start loading data");
+        String s = null;
+        int j;
+        long i = 0;
+        // TIMER START
+        TimerManage.start();
+        // Get Table
+        Table table = BasicStorage.getTable(csv_filename);
+        BufferedReader temp = new BufferedReader(new InputStreamReader(input));
+        String firstLine = temp.readLine();
+        temp.close();
+        for(String column_name: table.getColumns().keySet()) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(input));
+            s = in.readLine();  s = in.readLine();
+            // load lines
+            i=0;
+            j = getColumnNumber(column_name,firstLine);
+            while ((s = in.readLine()) != null && i < max_size) {
+                if (i % 100000 == 0 && i != 0) {
+                    System.out.println(i + " lines inserted");
+                }
+                String[] line = s.split(",");
+                addElement(table,column_name,line[j]);
+                i++;
+            }
+            j++;
+            in.close();
+        }
+        System.out.println(i  + " lines have been inserted successfully");
+        table.setSize(table.getSize()+i);
+        // TIMER END
+        TimerManage.pause();
+        String Time = "Elapsed Time : " + TimerManage.getTime()+" ms\n";
+        Time += "Lines inserted : " + i;
+        System.out.println(Time);
+        return Time;
+    }
+
+    public static int getColumnNumber(String column_name, String firstLine){
+        List<String> head = Arrays.asList(firstLine.split(","));
+        return head.indexOf(column_name);
+
+    }
+
+    public static void addElement(Table table,String column_name, String data) {
+        String type = table.getColumns().get(column_name).getType();
+        switch (type) {
+            case "float":
+                ColumnFloat colFloat = (ColumnFloat) table.getColumns().get(column_name);
+                colFloat.add(Float.parseFloat(data));
+                break;
+            case "String":
+                ColumnString colString = (ColumnString) table.getColumns().get(column_name);
+                colString.add(data);
+                return;
+            case "byte":
+                ColumnByte colByte = (ColumnByte) table.getColumns().get(column_name);
+                colByte.add(Byte.parseByte(data));
+                break;
+            case "int":
+                ColumnInt colInt = (ColumnInt) table.getColumns().get(column_name);
+                colInt.add(Integer.parseInt(data));
+                break;
+            case "double":
+                ColumnDouble colDouble = (ColumnDouble) table.getColumns().get(column_name);
+                colDouble.add(Double.parseDouble(data));
+                break;
+            case "short":
+                ColumnShort colShort = (ColumnShort) table.getColumns().get(column_name);
+                colShort.add(Short.parseShort(data));
+                break;
+            case "long":
+                ColumnLong colLong = (ColumnLong) table.getColumns().get(column_name);
+                colLong.add(Long.parseLong(data));
+                break;
+            case "boolean":
+                ColumnBoolean colBoolean = (ColumnBoolean) table.getColumns().get(column_name);
+                colBoolean.add(Boolean.parseBoolean(data));
+                break;
+            default:
+                System.out.println("Type non pris en charge.");
+                break;
+        }
+    }
+
 }
