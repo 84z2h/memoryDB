7c6d47c03f2941e02e995f0f0cbd1f6036e38d8d
84z2h <90423456+84z2h@users.noreply.github.com>
2022-06-01 21:40:09 +0200

    up

--- src/main/java/com/dant/entity/ServiceClient.java
+++ src/main/java/com/dant/entity/ServiceClient.java
@@ -3,6 +3,7 @@ package com.dant.entity;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
 public class ServiceClient {
@@ -13,7 +14,6 @@ public class ServiceClient {
         c1 = ClientBuilder.newClient();
         c2 = ClientBuilder.newClient();
     }
-
     @Override
     public String toString() {
         return "ServiceClient{" +
@@ -24,19 +24,18 @@ public class ServiceClient {
     public Client getC1() {return c1;}
     public Client getC2() {return c2;}
 
-    public String clientGET(String s){
-
-        String res = c1.target(s).request(MediaType.APPLICATION_JSON).get(String.class);
-        res += c2.target(s).request(MediaType.APPLICATION_JSON).get(String.class);
+    public String clientGET(String ip1, String ip2, String uri){
+        String res = c1.target("https://"+ip1+":"+"8080"+uri).request(MediaType.APPLICATION_JSON).get(String.class);
+        res += c2.target("https://"+ip2+":"+"8080"+uri).request(MediaType.APPLICATION_JSON).get(String.class);
         return res;
     }
-    public void clientPUT(String url, Object obj){
-        c1.target(url).request(MediaType.APPLICATION_JSON).put(Entity.json(obj));
-        c2.target(url).request(MediaType.APPLICATION_JSON).put(Entity.json(obj));
+    public void clientPUT(String ip1, String ip2, String uri, Object obj){
+        c1.target("https://"+ip1+":"+"8080"+uri).request(MediaType.APPLICATION_JSON).put(Entity.json(obj));
+        c2.target("https://"+ip2+":"+"8080"+uri).request(MediaType.APPLICATION_JSON).put(Entity.json(obj));
     }
-    public void clientPOST(String url, Object obj){
-        c1.target(url).request(MediaType.APPLICATION_JSON).post(Entity.json(obj));
-        c2.target(url).request(MediaType.APPLICATION_JSON).post(Entity.json(obj));
+    public void clientPOST(String ip1, String ip2, String uri, Object obj){
+        c1.target("https://"+ip1+":"+"8080"+uri).request(MediaType.APPLICATION_JSON).post(Entity.json(obj));
+        c2.target("https://"+ip2+":"+"8080"+uri).request(MediaType.APPLICATION_JSON).post(Entity.json(obj));
     }
     public void closeClients(){
         c1.close();
