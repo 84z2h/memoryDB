2d38c648cf0dcda563af66f398a682bc408d9d8a
84z2h <90423456+84z2h@users.noreply.github.com>
2022-06-09 14:21:44 +0200

    commit inutile

--- src/main/java/com/dant/entity/distribution/ServiceClient.java
+++ src/main/java/com/dant/entity/distribution/ServiceClient.java
@@ -33,8 +33,8 @@ public class ServiceClient {
         providerFactory.registerProvider(GsonProvider.class);
         client = new ResteasyClientBuilder().withConfig(providerFactory).build();
 
-        nodes_Name.add("http://192.168.1.56:8081");
-        //nodes_Name.add("ip3");
+        //nodes_Name.add("http://132.227.124.73:8081");
+        nodes_Name.add("http://132.227.124.74:8082");
     }
 
     public static int getNbNode(){
